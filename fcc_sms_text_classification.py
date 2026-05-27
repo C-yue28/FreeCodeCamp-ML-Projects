b8a9167d1d9caa4f80dadd9d8d7f94f8f5e4cdf8
C-yue28 <chrisyue321@gmail.com>
2026-05-27 08:10:36 -0400

    Update fcc_sms_text_classification.py

--- fcc_sms_text_classification.py
+++ fcc_sms_text_classification.py
@@ -10,3 +10,63 @@ from tensorflow import keras
 import tensorflow_datasets as tfds
 import numpy as np
 import matplotlib.pyplot as plt
+
+!wget https://cdn.freecodecamp.org/project-data/sms/train-data.tsv
+!wget https://cdn.freecodecamp.org/project-data/sms/valid-data.tsv
+
+train_file_path = "train-data.tsv"
+test_file_path = "valid-data.tsv"
+
+# cell 3: clean up data
+
+train_dataset = tf.data.experimental.CsvDataset(
+    filenames=file_path,
+    record_defaults=record_defaults,
+    header=False,     # Skips header or assumes no header is present
+    field_delim='\t'  # Specifies Tab separation
+)
+
+# cell 4
+
+
+
+# cell 5
+
+def predict_message(pred_text):
+    
+    # Model logic goes here
+    
+    return (prediction)
+
+pred_text = "how are you doing today?"
+
+prediction = predict_message(pred_text)
+print(prediction)
+
+
+
+def test_predictions():
+    test_messages = [
+        "how are you doing today",
+        "sale today! to stop texts call 98912460324",
+        "i dont want to go. can we try it a different day? available sat",
+        "our new mobile video service is live. just install on your phone to start watching.",
+        "you have won £1000 cash! call to claim your prize.",
+        "i ll bring it tomorrow. don t forget the milk.",
+        "wow, is your arm alright. that happened to me one time too",
+    ]
+
+    test_answers = ["ham", "spam", "ham", "spam", "spam", "ham", "ham"]
+    passed = True
+
+    for msg, ans in zip(test_messages, test_answers):
+        prediction = predict_message(msg)
+        if prediction[1] != ans:
+            passed = False
+
+    if passed:
+        print("You passed the challenge. Great job!")
+    else:
+        print("You haven't passed yet. Keep trying.")
+
+test_predictions()
